fdb5baf6f86d7f035f7c50b62dd63d22ee8c04fb
lisvega <lissyrivera456@gmail.com>
2022-09-30 14:27:18 +0200

    ejercicio operadores

--- JAVASCRIPT/js-basic-algorithms/App.js
+++ JAVASCRIPT/js-basic-algorithms/App.js
@@ -63,3 +63,35 @@ car2.finalPrice = car2.basePrice + globalBasePrice;
 console.log(car2);
 
 
+
+//Iteración #3: Operadores
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+let Multiplicación = 10 * 5
+console.log(Multiplicación);
+
+//1.2 Divide 10 por 2 y muestra el resultado en un console.
+let division = 10 / 2
+console.log(division);
+
+//1.3 Muestra mediante un console el resto de dividir 15 por 9.
+let resto = 15 % 9
+console.log(resto)
+//1.4 Usa el correcto operador de asignación que resultará en o = 15,
+//teniendo dos variables p = 10 y j = 5.
+
+let p = 10
+let j = 5
+
+let o = p + j
+console.log(o)
+
+//1.5 Usa el correcto operador de asignación que resultará en i = 50,
+//teniendo dos variables c = 10 y m = 5.
+
+let c = 10
+let m = 5
+
+let i = 10 * 5
+console.log(i)
+
+
